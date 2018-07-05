0292fa05115560bfb4cc54bc9692e8ec1d2cb7a4
吹田洋輔 <suita@mba-2.local>
2018-07-05 21:04:05 +0900

    Setting config for heroku 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  host = 'kiniita.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => host }
+  config.action_mailer.default_url_options = { :host => 'kiniita.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
