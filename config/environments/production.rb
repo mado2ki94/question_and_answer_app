8634fee86326bef6a39014016c9ca96c6568feb6
吹田洋輔 <suita@mba-2.local>
2018-07-05 20:32:55 +0900

    Setting config file for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'kiniita.herokuapp.com' }
+  host = 'kiniita.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => host }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
