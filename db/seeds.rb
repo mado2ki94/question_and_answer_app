4749c6a82c014aff0341a6bd67f822e710162f9e
吹田洋輔 <suita@mba-2.local>
2018-07-08 21:26:10 +0900

    Modify db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ User.create!(name:  "Example User",
              password:              "password",
              password_confirmation: "password")
 
+# ユーザーを50人作成
 50.times do |n|
  name  = Faker::Name.name
  email = "example-#{n+1}@railstutorial.org"
@@ -13,9 +14,28 @@ User.create!(name:  "Example User",
               password_confirmation: password)
 end
 
+# 全てのユーザーが一つずつ質問を投稿
 users = User.all
-title   = Faker::Lorem.sentence(6)
-content = Faker::Lorem.sentence(50)
 users.each do |user|
+  title   = Faker::Lorem.sentence(6)
+  content = Faker::Lorem.sentence(50)
   user.questions.create!(title: title ,content: content, resolution: 0)
+end
+
+# 全てのユーザーがランダムに一つの質問に回答
+users = User.all
+users.each do |user|
+  content = Faker::Lorem.sentence(30)
+  question_id = rand(1..51)
+  user.answers.create(content: content, user_id: user.id, question_id: question_id)
+end
+
+全てのユーザーがランダムに三つの回答にいいね
+users = User.all
+3.times do
+ users.each do |user|
+   answer_id = rand(1..51)
+   answer = Answer.find_by(answer_id: answer_id)
+   user.like(answer)
+ end
 end
