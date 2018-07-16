1bffca7dc587bb37942abf681ee188f084263f53
吹田洋輔 <suita@mba-2.local>
2018-07-16 14:02:37 +0900

    test devise in heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,14 +31,24 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # gem deviseの設定
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.default_url_options[:host] = 'localhost:3000'
 
   # 認証メールをブラウザで確認
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
+  user_name = ENV['GMAIL_USERNAME']
+  password = ENV['GMAIL_APP_PASSWORD']
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => user_name,
+    :password => password,
+    :authentication => 'login'
+  }
 
   config.action_mailer.perform_caching = false
 
