8c4bbaac68ce6986964ffa702c31372d0ee2780c
吹田洋輔 <suita@mba-2.local>
2018-07-16 14:24:25 +0900

    test devise in heroku

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module MyApps
     config.i18n.default_locale = :ja
 
     # 認証トークンをremoteフォームに埋め込む
-    # config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = true
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "My_apps_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { :host => 'fiita.herokuapp.com' }
+  host = 'fiita.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => host }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
